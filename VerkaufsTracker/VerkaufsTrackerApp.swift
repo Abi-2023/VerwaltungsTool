d9fb0d24a69b0267b597a6c5f7f1d229cc258367
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-13 16:19:21 +0100

    init

---
+++ VerkaufsTracker/VerkaufsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerkaufsTrackerApp.swift
+//  
+//
+//  Created by Benedict on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct VerkaufsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
