30d65516b62202b60de5784f7c9e7243365ca8a2
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-16 22:03:11 +0100

    scan and generate QR-Codes

--- VerkaufsTracker/ContentView.swift
+++ VerkaufsTracker/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+#if canImport(CodeScanner)
+import CodeScanner
+#endif
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+	@State private var isShowingScanner = false
+	@State var image: UIImage?
+
+
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundColor(.accentColor)
+			Text("Hello, world!")
+		}
+		.padding()
+		.onAppear {
+			var x = QrCodeImage()
+			image = x.generateQRCode(from: "abc")
+			print(image)
+		}
+
+		if let image2 = image {
+			Image(uiImage: image2)
+				.resizable()
+//				.scaleEffect(2)
+//				.foregroundColor(.blue)
+//				.background(.red)
+				.scaledToFit()
+		}
+
 
 		Button(action: {
 			let mailer = EmailManager()
@@ -23,11 +45,56 @@ struct ContentView: View {
 		}) {
 			Text("Send Email")
 		}
+
+#if canImport(CodeScanner)
+		Button(action: {
+			isShowingScanner = true
+		}) {
+			Text("Scan")
+		}
+		.sheet(isPresented: $isShowingScanner) {
+			CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+		}
+		.padding()
+
+
+
+
+
+#endif
     }
+
+	func handleScan(result: Result<ScanResult, ScanError>) {
+	   isShowingScanner = false
+
+		print(result)
+	   // more code to come
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct QrCodeImage {
+    let context = CIContext()
+
+    func generateQRCode(from text: String) -> UIImage {
+        var qrImage = UIImage(systemName: "xmark.circle") ?? UIImage()
+        let data = Data(text.utf8)
+        let filter = CIFilter(name: "CIQRCodeGenerator")!
+        filter.setValue(data, forKey: "inputMessage")
+
+        let transform = CGAffineTransform(scaleX: 20, y: 20)
+        if let outputImage = filter.outputImage?.transformed(by: transform) {
+            if let image = context.createCGImage(
+                outputImage,
+                from: outputImage.extent) {
+                qrImage = UIImage(cgImage: image)
+            }
+        }
+        return qrImage
     }
 }
