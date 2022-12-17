f6b6a6a25947fb4faaeb8b084514b944137ee318
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-17 20:04:24 +0100

    Person add form id

--- VerkaufsTracker/Structs/Person.swift
+++ VerkaufsTracker/Structs/Person.swift
@@ -20,8 +20,9 @@ class Person: Sync {
 	var notes: String
 	var bestellungen: [UUID: Int]
 	var extraFields: [String: String]
+	var formID: String
 
-	init(id: UUID, isSynced: Bool, lastUpdate: Date, lastServerEdit: Date, vorname: String, nachname: String, email: String? = nil, q2: Bool, notes: String, bestellungen: [UUID : Int], extraFields: [String : String]) {
+	init(id: UUID, isSynced: Bool, lastUpdate: Date, lastServerEdit: Date, vorname: String, nachname: String, email: String? = nil, q2: Bool, notes: String, bestellungen: [UUID : Int], extraFields: [String : String], verwaltung: Verwaltung) {
 		self.id = id
 		self.isSynced = isSynced
 		self.lastUpdate = lastUpdate
@@ -33,5 +34,28 @@ class Person: Sync {
 		self.notes = notes
 		self.bestellungen = bestellungen
 		self.extraFields = extraFields
+		self.formID = verwaltung.generateFormId()
+	}
+}
+
+class Verwaltung {
+
+	var personen: [Person] = []
+
+	func generateFormId() -> String{
+		//regex: /[ABCDEF][175963][SEFWQX][MNDQS5][W3YJ52]/
+		let options = [
+			["A", "B", "C", "D", "E", "F"],
+			["1", "7", "5", "9", "6", "3"],
+			["S", "E", "F", "W", "Q", "X"],
+			["M", "N", "D", "Q", "S", "5"],
+			["W", "3", "Y", "J", "5", "2"],
+		]
+		while true {
+			let formId = options.map({$0.randomElement()!}).reduce("", +)
+			if !personen.map({$0.formID}).contains(formId) {
+				return formId
+			}
+		}
 	}
 }
