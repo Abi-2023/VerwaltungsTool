f8a6d45815674dbef3508b38fa9424b5d43fe7ce
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-18 20:21:32 +0100

    Personen Detail View

--- VerkaufsTracker/Views/PersonDetailView.swift
+++ VerkaufsTracker/Views/PersonDetailView.swift
@@ -8,7 +8,41 @@
 import SwiftUI
 
 struct PersonDetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@Binding var person: Person?
+	@State private var formEmailConfirmationShown = false
+
+
+	var body: some View {
+		Button(action: {
+			person = nil
+		}) {
+			Text("close")
+		}
+		if let p = person {
+			Text(p.vorname)
+			Text(p.nachname)
+			Text(p.email ?? "keine Email hinterlegt")
+			Text(p.q2 ? "in Q2" : "nicht in Q2")
+
+			Text(p.notes)
+
+			Button(role: .destructive, action: {
+				formEmailConfirmationShown = true
+			}) {
+				Text("Form Email senden")
+			}
+			.confirmationDialog(
+				"Email versenden?",
+				 isPresented: $formEmailConfirmationShown
+			) {
+				Button("Ja, senden!") {
+					let mm = EmailManager()
+					mm.sendMail(mail: mm.generateFormEmail(person: p))
+				}
+			}
+			// TODO: einfügen
+//			var bestellungen: [UUID: Int]
+//			var extraFields: [String: String]
+		}
+	}
 }
--- VerkaufsTracker/Views/PersonenView.swift
+++ VerkaufsTracker/Views/PersonenView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct PersonenView: View {
 	@State var verwaltung: Verwaltung
+	@State var selectedPerson: Person?
     var body: some View {
 		Text("Suchen") // TODO: searchbar
 		List(verwaltung.personen) { person in
 			Text(person.vorname)
+				.onTapGesture {
+					selectedPerson = person
+				}
 		}
-    }
+		.sheet(item: $selectedPerson) { _ in
+			PersonDetailView(person: $selectedPerson)
+				.interactiveDismissDisabled(true)
+		}
+	}
 }
