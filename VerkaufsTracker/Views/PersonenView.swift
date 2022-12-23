67453343a12b5d2525babcb23a5df895686115b9
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-23 23:37:46 +0100

    PersonenView filter by groups

--- VerkaufsTracker/Views/PersonenView.swift
+++ VerkaufsTracker/Views/PersonenView.swift
@@ -10,10 +10,38 @@ import SwiftUI
 struct PersonenView: View {
 	@State var verwaltung: Verwaltung
 	@State var selectedPerson: Person?
+
+	enum GruppenTypen: String {
+		case _Alle
+		case _Q2er
+		case _Lehrer
+
+		var type: Any.Type {
+			switch self {
+			case ._Alle:
+				return Person.self
+			case ._Q2er:
+				return Q2er.self
+			case ._Lehrer:
+				return Lehrer.self
+			}
+		}
+	}
+
+	@State var gruppenTyp: GruppenTypen = ._Alle
+
 	var body: some View {
 		Text("Suchen") // TODO: searchbar
+		HStack {
+			Picker("a", selection: $gruppenTyp) {
+				Text("Alle").tag(GruppenTypen._Alle)
+				Text("Q2").tag(GruppenTypen._Q2er)
+				Text("Lehrer").tag(GruppenTypen._Lehrer)
+			}
+
+		}
 		List {
-			ForEach(verwaltung.personen, id: \.self) { person in
+			ForEach(verwaltung.personen.filter({type(of: $0) == gruppenTyp.type || gruppenTyp == ._Alle}), id: \.self) { person in
 				Text(person.notes)
 					.onTapGesture {
 //						selectedPerson = person
