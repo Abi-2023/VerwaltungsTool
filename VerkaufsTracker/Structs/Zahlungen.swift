7e0d120b93fd5b5311cd383b39083e12b6beea1a
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-25 19:01:21 +0100

    add Transaktion class

--- VerkaufsTracker/Structs/Zahlungen.swift
+++ VerkaufsTracker/Structs/Zahlungen.swift
@@ -7,3 +7,27 @@
 
 import Foundation
 
+// MARK: Alle Beträge in Cent
+// 1 = 1ct
+// 200 = 2€
+
+class Transaktion: Codable, Hashable {
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(betrag)
+		hasher.combine(beschreibung)
+		hasher.combine(person)
+
+	}
+
+	static func == (lhs: Transaktion, rhs: Transaktion) -> Bool {
+		lhs.betrag == rhs.betrag &&
+		lhs.beschreibung == rhs.beschreibung &&
+		lhs.person == rhs.person
+	}
+
+
+	let betrag: Int
+	let beschreibung: String
+	unowned let person: Person
+
+}
