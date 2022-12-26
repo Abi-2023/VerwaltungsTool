3ac5c2e3058f79f42c31ec42132843337094112d
Benedict <52418145+BenedictSt@users.noreply.github.com>
2022-12-26 20:39:56 +0100

    Stats fix form submitted

--- VerkaufsTracker/Views/StatsView.swift
+++ VerkaufsTracker/Views/StatsView.swift
@@ -14,7 +14,7 @@ struct StatsView: View {
 		let wunschTickets = verwaltung.personen.map({$0.wuenschBestellungen[.ball_ticket] ?? 0}).reduce(0, +)
 		Text("gewünschte Tickets: \(wunschTickets) / \(Item.ball_ticket.verfuegbar) (\((Int(Float(wunschTickets) / Float(Item.ball_ticket.verfuegbar)*100)))%)")
 
-		let formSubmitted = verwaltung.personen.filter({$0.extraFields["sendFormEmail", default: ""] == "1"}).count
-		Text("gewünschte Tickets: \(formSubmitted) / \(verwaltung.personen.count) (\((Int(Float(formSubmitted) / Float(verwaltung.personen.count)*100)))%)")
+		let formSubmitted = verwaltung.personen.filter({$0.extraFields["hatFormEingetragen", default: ""] == "1"}).count
+		Text("Form ausgefüllt: \(formSubmitted) / \(verwaltung.personen.count) (\((Int(Float(formSubmitted) / Float(verwaltung.personen.count)*100)))%)")
 	}
 }
