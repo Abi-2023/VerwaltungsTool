2d7db30c3bbd3c4f3b98e6b9e3ad0c1313b05ecd
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-01-13 16:53:24 +0100

    lehrer neue attribute

--- VerkaufsTracker/Structs/Person/Lehrer.swift
+++ VerkaufsTracker/Structs/Person/Lehrer.swift
@@ -10,7 +10,42 @@ import SwiftSMTP
 
 class Lehrer: Person {
 
-	
+	let vorname: String
+	let nachname: String
+	let kuerzel: String
+	let weiblich: Bool
+
+	override var name: String { get {"\(vorname) \(nachname)"} set {}}
+	override var formName: String {kuerzel}
+
+	init(vorname: String, nachname: String, kuerzel: String, weiblich: Bool, v: Verwaltung) {
+		self.vorname = vorname
+		self.nachname = nachname
+		self.kuerzel = kuerzel
+		self.weiblich = weiblich
+		// TODO: richtige Email
+		//\(kuerzel)@st-anna.de
+		super.init(name: "", email: "***REMOVED***", verwaltung: v)
+	}
+
+	private enum CodingKeys: String , CodingKey {case vorname, nachname, kuerzel, weiblich}
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		vorname = try container.decode(String.self, forKey: .vorname)
+		nachname = try container.decode(String.self, forKey: .nachname)
+		kuerzel = try container.decode(String.self, forKey: .kuerzel)
+		weiblich = try container.decode(Bool.self, forKey: .weiblich)
+		try super.init(from: decoder)
+	}
+
+	override func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		try super.encode(to: encoder)
+		try container.encode(vorname, forKey: .vorname)
+		try container.encode(nachname, forKey: .nachname)
+		try container.encode(kuerzel, forKey: .kuerzel)
+		try container.encode(weiblich, forKey: .weiblich)
+	}
 
 
 	static func == (lhs: Lehrer, rhs: Lehrer) -> Bool {
