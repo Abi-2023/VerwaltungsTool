d593358290c15d3b9249db29beee520f97b7591d
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-01-22 15:16:43 +0100

    CSV auswaehlen

--- VerkaufsTracker/Views/ZahlungsImportOptionen.swift
+++ VerkaufsTracker/Views/ZahlungsImportOptionen.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+enum MyError: Error {
+	case runtimeError(String)
+}
+
 struct ZahlungsImportOptionen: View {
 	@ObservedObject var zahlungsVerarbeiter: ZahlungsVerarbeiter
+
+	@State var showDocumentPicker = true
+
 	var body: some View {
-		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Text("Hello, World!")
+			.fileImporter(isPresented: $showDocumentPicker,
+						  allowedContentTypes: [.commaSeparatedText],
+								  allowsMultipleSelection: false)
+					{ result in
+						do {
+							guard let file = try result.get().first else {
+								throw MyError.runtimeError("keine Datei ausgewählt")
+							}
+
+							let data = try Data(contentsOf: file)
+							guard let fileStr = String(data: data, encoding: .utf8) else {
+								throw MyError.runtimeError("Datei konnte nicht gelesen werden")
+							}
+							print(fileStr)
+
+						} catch {
+							print("error: \(error)")
+						}
+
+					}
 	}
 }
