5034f7c7be39de8b7eaf7b8bab615c34cc28859c
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-01-31 20:12:33 +0100

    fix preiseFuerItem static

--- VerkaufsTracker/EmailsGenerieren/Email_LE_Bezahl.swift
+++ VerkaufsTracker/EmailsGenerieren/Email_LE_Bezahl.swift
@@ -29,7 +29,7 @@ extension Lehrer {
   <tr>
    <td style="padding: 0 15px 0 0;" align="left">\(value)x</td>
    <td style="padding: 0 " align="left">\(itemType.rechnungsPosition)</td>
-   <th style="padding: 0 0 0 15px;"align="right">\(preisFuerItem(item: itemType, count: value).geldStr)</th>
+   <th style="padding: 0 0 0 15px;"align="right">\(Person.preisFuerItem(item: itemType, count: value).geldStr)</th>
   </tr>
   """
 
